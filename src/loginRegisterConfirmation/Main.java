8d5779a4b0017c840468d8a2829d159172827435
sumeyye <sumeyyeyegen746@gmail.com>
2021-05-07 15:40:52 +0300

    Main class'ında signUp methodu kullanıldı.

--- src/loginRegisterConfirmation/Main.java
+++ src/loginRegisterConfirmation/Main.java
@@ -7,7 +7,13 @@ import loginRegisterConfirmation.entities.concretes.User;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		User user = new User();
+		user.setFirstName("sum");
+		user.setLastName("yeg");
+		user.setEmail("sumeyyeyegen@gmail.com");
+		user.setPassword("123456");
+		UserManager userManager = new UserManager(new HibernateUserDao());
+		userManager.signUp(user);
 	}
 
 }
