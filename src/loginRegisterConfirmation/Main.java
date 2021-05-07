0409a6959f29d4144ceb295a9ca84a33c75c0bb8
sumeyye <sumeyyeyegen746@gmail.com>
2021-05-07 14:52:51 +0300

    Main class'ı eklendi.

---
+++ src/loginRegisterConfirmation/Main.java
@@ -0,0 +1,13 @@
+package loginRegisterConfirmation;
+
+import loginRegisterConfirmation.business.concretes.UserManager;
+import loginRegisterConfirmation.dataAccess.concretes.HibernateUserDao;
+import loginRegisterConfirmation.entities.concretes.User;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
