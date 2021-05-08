d3e84d2d4c31e7529188efa08cfce7d42cec9a6d
sumeyye <sumeyyeyegen746@gmail.com>
2021-05-08 14:53:51 +0300

    UserDao class'ında kullanılacak methodlar tanımlandı.

--- src/loginRegisterConfirmation/dataAccess/abstracts/UserDao.java
+++ src/loginRegisterConfirmation/dataAccess/abstracts/UserDao.java
@@ -1,8 +1,12 @@
 package loginRegisterConfirmation.dataAccess.abstracts;
 
+import java.util.List;
+
 import loginRegisterConfirmation.entities.concretes.User;
 
 public interface UserDao {
 	void add(User user);
-	void login(String email,String password);
+	void delete(User user);
+	boolean getControl(String email);
+	List<User> getUser();
 }
