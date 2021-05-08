54356c3126e8aee404b0beadc930280fe814f157
sumeyye <sumeyyeyegen746@gmail.com>
2021-05-08 14:54:52 +0300

    UserDao interface'inden implement edilen methodların gövdeleri dolduruldu.

--- src/loginRegisterConfirmation/dataAccess/concretes/HibernateUserDao.java
+++ src/loginRegisterConfirmation/dataAccess/concretes/HibernateUserDao.java
@@ -1,18 +1,39 @@
 package loginRegisterConfirmation.dataAccess.concretes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import loginRegisterConfirmation.dataAccess.abstracts.UserDao;
 import loginRegisterConfirmation.entities.concretes.User;
 
 public class HibernateUserDao implements UserDao {
+	
+	List <User> users = new ArrayList<User>();
 
 	@Override
 	public void add(User user) {
+		users.add(user);
 		System.out.println("Kullanici hibernate ile eklendi. ");
 	}
 
 	@Override
-	public void login(String email, String password) {
-		
+	public void delete(User user) {
+		user = users.remove(user.getId());
+		System.out.println(user.getFirstName() + " isimli kullanici silindi.");
+	}
+
+	@Override
+	public boolean getControl(String email) {
+		for(User user:users) {
+			if(user.getEmail().equals(email)) {
+				return true;
+			}
+		}return false;
+	}
+
+	@Override
+	public List<User> getUser() {
+		return users;
 	}
 	
 }
